575ed223958b7d9a1f1345e7d4be76a74824473f
Mafiyozist <chaneldinara@gmail.com>
2020-10-26 09:49:28 +0700

    added adapter and ViewHolder for CrimeListFragment

--- app/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -1,11 +1,6 @@
 package com.example.criminalintent;
-
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+
 
 public class CrimeActivity extends SingleFragmentActivity {
     @Override
